11a925128767c86e5d37b513619a6e5c99de9da4
alvalle09 <phredvalle@gmail.com>
2018-08-01 00:25:09 -0500

    final js code, disble room input when no room selected, pick room before submit message

--- src/Components/MessageList.js
+++ src/Components/MessageList.js
@@ -24,11 +24,20 @@ class MessageList extends React.Component {
     }
 
     render() {
+        if (!this.props.roomId) {
+            return (
+                <div className="message-list">
+                    <div className="join-room">
+                        &larr; Join a room!
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="message-list">
                {this.props.messages.map(( message , index) => {
                     return (
-                        <Message key={index} username={message.senderId} text={message.text} />                        
+                        <Message key={message.id} username={message.senderId} text={message.text} />                        
                     )
                })}
             </div>
--- src/Components/NewRoomForm.js
+++ src/Components/NewRoomForm.js
@@ -21,6 +21,7 @@ class NewRoomForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault()
         this.props.createRoom(this.state.roomName)
+        this.setState({roomName: ''})
     }
 
     render () {
@@ -28,9 +29,10 @@ class NewRoomForm extends React.Component {
             <div className="new-room-form">
                 <form onSubmit={this.handleSubmit}>
                     <input
+                        value={this.state.roomName}
                         onChange={this.handleChange}
                         type="text" 
-                        placeholder="NewRoomForm" 
+                        placeholder="Create a Room" 
                         required />
                     <button id="create-room-btn" type="submit">+</button>
             </form>
